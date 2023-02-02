05a05c109d8f976ed04af5bb4620819a9324086b
vassili45 <vassili.papaodpoulos45@gmail.com>
2023-02-02 12:26:35 +0100

    ajout de la route project par id

--- backend/src/router.js
+++ backend/src/router.js
@@ -4,6 +4,15 @@ const router = express.Router();
 
 const projectControllers = require("./controllers/projectControllers");
 
+// ----------- EXEMPLE DES ROUTES ------------
+// router.get("/items", itemControllers.browse);
+// router.get("/items/:id", itemControllers.read);
+// router.put("/items/:id", itemControllers.edit);
+// router.post("/items", itemControllers.add);
+// router.delete("/items/:id", itemControllers.destroy);
+// --------------------------------------------
+
 router.get("/project", projectControllers.browse);
+router.get("/project/:id", projectControllers.read);
 
 module.exports = router;
