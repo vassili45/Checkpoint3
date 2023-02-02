128d25fc68d769e008721e943675e845dbe50084
vassili45 <vassili.papaodpoulos45@gmail.com>
2023-02-02 11:55:02 +0100

    creation database et ajout d'un png pour la db

--- backend/database.sql
+++ backend/database.sql
@@ -1,6 +1,17 @@
-CREATE TABLE item (
+CREATE TABLE utilisateur (
   id int(11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  title varchar(255) NOT NULL
+  email varchar(255) NOT NULL,
+  password varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO item (title) VALUES ('Stuff'), ('Doodads');
+INSERT INTO utilisateur (id, email, password) VALUES (1, 'admin@admin.com', 'admin123');
+
+CREATE TABLE project (
+  id int(11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  Nom varchar(255) NOT NULL,
+  Url VARCHAR(1000) NOT NULL,
+  Screenshot VARCHAR(1000),
+  Description VARCHAR(1000)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO project (id, Nom, Url, Screenshot, Description) VALUES (1, 'Origins Digitals', 'https://origins.remote-fr-3.wilders.dev/', '/uploads/Origins_Digitals.png', 'Ce project ai réaliser en 3 mois');
