400c26bbca4f9ec497701be154791375254fdb98
vassili45 <vassili.papaodpoulos45@gmail.com>
2023-02-02 15:53:44 +0100

    creation de la nav

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <nav>
+      <ul>
+        <div className="flex justify-end gap-4 items-center text-2xl bg-black">
+          <Link to="/">HOME</Link>
+          <Link to="/AllProject">Tout les project</Link>
+          <Link to="/Dashboard/Project">Dashboard</Link>
+          <Link to="/Login">Login</Link>
+        </div>
+      </ul>
+    </nav>
+  );
 }
 
 export default NavBar;
